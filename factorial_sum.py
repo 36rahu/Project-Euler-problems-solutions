e0f2941b543b20d9f61acf3140b48187c856ac7c
Rahul KP <36rahu@users.noreply.github.com>
2014-11-26 00:52:36 +0530

    Update factorial_sum.py

--- factorial_sum.py
+++ factorial_sum.py
@@ -1,3 +1,13 @@
+"""
+Question:
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
 def factorial_sum():
     j,su = 0,1
     for i in range(1,101):
@@ -12,4 +22,3 @@ def factorial_sum():
 if __name__ == '__main__':
     digit_sum = factorial_sum()
     print "Sum of Factorial umber 100! =",digit_sum
-
